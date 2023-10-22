9b03253d57efc6b9a819d5311ed4ccda23e3f966
bulat1337 <bulatmot@gmail.com>
2023-10-22 18:08:57 +0300

    ugly binary asm w extreme aligning

--- assembly/assembly.cpp
+++ assembly/assembly.cpp
@@ -6,8 +6,16 @@
 #include "../buffer_process/buffer_process.h"
 #include "../stack/stack.h"
 
+struct Buf_w_carriage_n_len
+{
+	char *buf;
+	size_t carriage;
+	size_t length;
+};
+
+void write_to_buf(struct Buf_w_carriage_n_len *byte_code, const void *value, size_t size);
 void aligned_fwrite(const int command_value, FILE *dest_file);
-void aligned_buf_print(const int command_value, char *buf);
+void aligned_buf_print(const int command_value, char *buf, size_t *buf_carriage);
 
 int main()
 {
@@ -35,18 +43,18 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	struct Buffer_w_info human_cmds_n_len = {};
-	struct Buffer_w_info byte_code_n_len = {};
 	size_t file_length = get_file_length(human_commands);
+	struct Buffer_w_info human_cmds_n_len = {};
 	human_cmds_n_len.length = file_length;
-	byte_code_n_len.length = file_length;
 	human_cmds_n_len.buf = (char *)calloc(human_cmds_n_len.length, sizeof(char));
-	byte_code_n_len.buf = (char *)calloc(human_cmds_n_len.length, sizeof(char));
-
 	if(human_cmds_n_len.buf == NULL)
 	{
 		fprintf(stderr, "Not enough memory for allocation.\n");
 	}
+
+
+
+
 
 	size_t read_elems_amount =
 		fread(human_cmds_n_len.buf, sizeof(char), human_cmds_n_len.length, human_commands);
@@ -74,12 +82,20 @@ int main()
 	char * *cmd_ptrs = (char * *)calloc(amount_of_lines, sizeof(char *));
 	ptr_arranger(cmd_ptrs, human_cmds_n_len);
 
+	struct Buf_w_carriage_n_len byte_code = {};
+	byte_code.length = file_length;
+	byte_code.buf = (char *)calloc(amount_of_lines * sizeof(double) * 2 + 1, sizeof(char));
+
 
 	const size_t buffer_size = 228; //может задать размер как максимальную длну комнады в файле??
+	const int immediate_const_identifier = 1;
+	const double void_alignment_value = 0;
 	char command_buffer[buffer_size] = {};
 
 	double argument_value = 0;
-	int reg_type = 0;
+	char reg_type = 0;
+	size_t buf_carriage = 0;
+	int command_code = 0;
 
 	for(size_t line_ID = 0; line_ID < amount_of_lines; line_ID++)
 	{
@@ -96,53 +112,116 @@ int main()
 				printf("reg detected\n");
 				sscanf(cmd_ptrs[line_ID] + strlen("push") ,"%s", command_buffer);
 
-				reg_type = command_buffer[1] - 'a' + 1;
-				fprintf(made_txt_byte_code, "%d ", reg_type);
+				reg_type = command_buffer[1] - 'a';
+				fprintf(made_txt_byte_code, "%d\n", reg_type);
 				printf("reg type: %d\n", reg_type);
 
-				snprintf(byte_code_n_len.buf, sizeof(int), "%d", PUSH);
+				command_code = PUSH;
+				write_to_buf(&byte_code, &command_code, sizeof(int));
 
-				snprintf(byte_code_n_len.buf, sizeof(int), "%d", reg_type);
+				command_code = ZERO;
+				write_to_buf(&byte_code, &command_code, sizeof(char));
+				write_to_buf(&byte_code, &reg_type, sizeof(char));
+				write_to_buf(&byte_code, &command_code, sizeof(char));
+				write_to_buf(&byte_code, &command_code, sizeof(char));
 
-				sscanf(cmd_ptrs[line_ID] + strlen("push") + strlen(command_buffer) + 1 ,
-					"%lf", &argument_value);//да простит меня бог за такое извращение....
-
-				printf("Here is whats going into bin file:\n%lf\n", argument_value);
-
-				snprintf(byte_code_n_len.buf, sizeof(int), "%lf", argument_value);
-
-				fprintf(made_txt_byte_code, "%.2lf\n", argument_value);
+				write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
 			}
 			else
 			{
-				aligned_buf_print(PUSH, byte_code_n_len.buf);
+				command_code = PUSH;
+				write_to_buf(&byte_code, &command_code, sizeof(int));
+
+				command_code = ZERO;
+				write_to_buf(&byte_code, &immediate_const_identifier, sizeof(char));
+				write_to_buf(&byte_code, &command_code, sizeof(char));
+				write_to_buf(&byte_code, &command_code, sizeof(char));
+				write_to_buf(&byte_code, &command_code, sizeof(char));
 
 				printf("Here is whats going into bin file:\n%lf\n", argument_value);
-
-				snprintf(byte_code_n_len.buf, sizeof(double), "%lf", argument_value);
+				write_to_buf(&byte_code, &argument_value, sizeof(double));
 
 				fprintf(made_txt_byte_code, "%.2lf\n", argument_value);
 			}
+		}
+		else if(!strncmp(command_buffer, "in", buffer_size))
+		{
+			fprintf(made_txt_byte_code, "%d\n", IN);
+
+			command_code = IN;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+			command_code = ZERO;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+
+			write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
+		}
+		else if(!strncmp(command_buffer, "pop", buffer_size))
+		{
+			printf("POP log:\n");
+
+			fprintf(made_txt_byte_code, "%d ", POP);
+
+			sscanf(cmd_ptrs[line_ID] + strlen("pop") ,"%s", command_buffer);
+
+			reg_type = command_buffer[1] - 'a';
+			fprintf(made_txt_byte_code, "%d\n", reg_type);
+			printf("reg type: %d\n", reg_type);
+
+			command_code = POP;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+
+			command_code = ZERO;
+			write_to_buf(&byte_code, &command_code, sizeof(char));
+			write_to_buf(&byte_code, &reg_type, sizeof(char));
+			write_to_buf(&byte_code, &command_code, sizeof(char));
+			write_to_buf(&byte_code, &command_code, sizeof(char));
+
+			write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
 		}
 		else if(!strncmp(command_buffer, "div", buffer_size))
 		{
 			fprintf(made_txt_byte_code, "%d\n", DIV);
-			aligned_buf_print(DIV, byte_code_n_len.buf);
+
+			command_code = DIV;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+			command_code = ZERO;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+
+			write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
 		}
 		else if(!strncmp(command_buffer, "sub", buffer_size))
 		{
 			fprintf(made_txt_byte_code, "%d\n", SUB);
-			aligned_buf_print(SUB, byte_code_n_len.buf);
+
+			command_code = SUB;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+			command_code = ZERO;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+
+			write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
 		}
 		else if(!strncmp(command_buffer, "out", buffer_size))
 		{
 			fprintf(made_txt_byte_code, "%d\n", OUT);
-			aligned_buf_print(OUT, byte_code_n_len.buf);
+
+			command_code = OUT;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+			command_code = ZERO;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+
+
+			write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
 		}
 		else if(!strncmp(command_buffer, "HLT", buffer_size))
 		{
 			fprintf(made_txt_byte_code, "%d\n", HLT);
-			aligned_buf_print(HLT, byte_code_n_len.buf);
+
+			command_code = HLT;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+			command_code = ZERO;
+			write_to_buf(&byte_code, &command_code, sizeof(int));
+
+			write_to_buf(&byte_code, &void_alignment_value, sizeof(double));
 		}
 		else
 		{
@@ -151,15 +230,48 @@ int main()
 
 	}
 
-	fwrite(byte_code_n_len.buf, sizeof(char), byte_code_n_len.length, made_bin_byte_code);
+	printf("----------------------------\n");
+	printf("IN BUF:\n");
+	for(size_t command_ID = 0; command_ID < amount_of_lines; command_ID++)
+	{
+		printf("COMMAND %lu:\n", command_ID + 1);
+
+		printf("command: %d\n", *(int *)(byte_code.buf + command_ID * sizeof(double) * 2));
+		printf("immediate const ID: %d\n",
+			*(char *)(byte_code.buf + sizeof(int) + command_ID * sizeof(double) * 2));
+		printf("register type: %d\n",
+			*(char *)(byte_code.buf + sizeof(int) + sizeof(char) + command_ID * sizeof(double) * 2));
+		printf("value: %lf\n",
+			*(double *)(byte_code.buf + sizeof(double) + command_ID * sizeof(double) * 2));
+
+		printf("\n");
+	}
+
+	printf("----------------------------\n");
+
 
 	return 0;
 }
 
 
-void aligned_buf_print(const int command_value, char *buf)
+void aligned_buf_print(const int command_value, char *buf, size_t *buf_carriage)
 {
 	snprintf(buf, sizeof(int), "%d", command_value);
+	*buf_carriage += sizeof(int);
 
 	snprintf(buf, sizeof(int), "%d", ZERO);
+	*buf_carriage += sizeof(int);
+}
+
+void write_to_buf(struct Buf_w_carriage_n_len *byte_code, const void *value, size_t size)
+{
+	char *byte_of_value = (char *)value;
+
+	for(size_t ID = 0; ID < size; ID++)
+	{
+		snprintf(byte_code->buf + byte_code->carriage, sizeof(char) + 1, "%c",
+			*(byte_of_value + ID));
+		byte_code->carriage += 1;
+	}
+
 }
